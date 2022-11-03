549f994a61e0e532f997629e7c4001f24d35a4ed
nagu758 <85338623+nagu758@users.noreply.github.com>
2022-11-03 14:35:37 +0530

    from math import* m = int(input()) n = int(input()) c = 0 l = [True]*(n+1) l[0],l[1] = False,False sq = int(sqrt(n)) for i in range(2,sq+1):     for j in range(i*i,n+1,i):         l[j]=False for i in range(m,n+1):     if l[i] == True:         c=c+1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,15 +1,14 @@
-from math import *
-m=int(input())
-n=int(input())
-c=0
-l=[True]*(n+1)
-l[0],l[1]=False,False
-sq=int(sqrt(n))
+from math import*
+m = int(input())
+n = int(input())
+c = 0
+l = [True]*(n+1)
+l[0],l[1] = False,False
+sq = int(sqrt(n))
 for i in range(2,sq+1):
     for j in range(i*i,n+1,i):
         l[j]=False
 for i in range(m,n+1):
-    if l[i]==True:
-        c+=1
+    if l[i] == True:
+        c=c+1
 print(c)
- 
