d0f9989c1b6bed64d8f115550619bc4307797744
nagu758 <85338623+nagu758@users.noreply.github.com>
2022-11-08 23:46:48 +0530

    def add(x):     s = 0     while x!=0:         r=x%10         s+=r         x=x//10     if len(str(s))==1:         print(s)     else:         add(s)
    n = int(input())
    add(n)

---
+++ Add_Digits.py
@@ -0,0 +1,13 @@
+def add(x):
+    s = 0
+    while x!=0:
+        r=x%10
+        s+=r
+        x=x//10
+    if len(str(s))==1:
+        print(s)
+    else:
+        add(s)
+    
+n = int(input())
+add(n)
